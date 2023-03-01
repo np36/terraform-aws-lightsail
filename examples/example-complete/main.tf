d05299f3af2ede1eb3b9a9f24da26fa9d93fd6fc
Nathan Pate <101912488+np36@users.noreply.github.com>
2023-03-01 08:13:44 +0800

    Update main.tf

--- examples/example-complete/main.tf
+++ examples/example-complete/main.tf
@@ -6,6 +6,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
+# If want to deploy Lightsail in a different region, create a alias provider like this
 provider "aws" {
   alias  = "lightsail"
   region = "ap-northeast-1"
